f2b1c7f4e1810aa8f239b0017f473eeac841bf1e
Pradeep <pradeepravi.ch@gmail.com>
2024-06-18 13:26:35 +0530

    env URLs changed

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
 export const ENVIRONMENT_ID = process.env.REACT_APP_ENVIRONMENT_ID || '';
 export const API_URL = process.env.REACT_APP_API_URL || '';
+export const API_LOCAL_URL = process.env.REACT_APP_API_LOCAL_URL || '';
+export const API_LIVE_URL = process.env.REACT_APP_API_LIVE_URL || '';
