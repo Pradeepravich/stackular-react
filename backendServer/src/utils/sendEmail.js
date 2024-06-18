e6691d259a85545ef4203b34f5387a41378d8444
Pradeep <pradeepravi.ch@gmail.com>
2024-06-18 10:48:58 +0530

    credentials updated

--- backendServer/src/utils/sendEmail.js
+++ backendServer/src/utils/sendEmail.js
@@ -10,14 +10,14 @@ exports.handler = async (event, context) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'pradeepapril91@gmail.com',
-      pass: 'avxjssculntkqlyh',
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
     },
   });
 
   const mailOptions = {
-    from: 'pradeepapril91@gmail.com',
-    to: 'pradeepjan89@gmail.com',
+    from: process.env.SMTP_USER,
+    to: process.env.EMAIL_TO,
     subject: `Contact form submission from ${name}`,
     text: message,
   };
